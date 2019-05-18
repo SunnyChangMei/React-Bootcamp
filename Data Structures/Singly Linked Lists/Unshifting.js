9a3e192f9825487655ef67ae2543efff026ad971
Sunny Chang Mei <mkaleader@gmail.com>
2019-05-18 18:39:42 -0400

    DS unshifting

---
+++ Data Structures/Singly Linked Lists/Unshifting.js
@@ -0,0 +1,95 @@
+/**
+ * ! Unshifting pseudocode
+ * 1. this function should accept a value
+ * 2. create a new node using the value passed to the function
+ * 3. if there is no head property on the list, set the head and tail to be the newly created node
+ * 4. otherwise set the newly created node's next property to be the current head property on the list
+ * 5. Set the head property on the list to be that newly created node
+ * 6. increment the length of the list by 1
+ * 7. return the linked list
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // traverse() {
+  //   let current = this.head;
+  //   while (current) {
+  //     console.log(current.val);
+  //     current = current.next;
+  //   }
+  // }
+  pop() {
+    if (this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (this.head) return undefined;
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
+
+console.log(list.push('Hello'));
+console.log(list.push('Goodbye'));
+console.log(list.push('99'));
+console.log(list.pop());
+console.log(list.shift());
+console.log(list.unshift());
