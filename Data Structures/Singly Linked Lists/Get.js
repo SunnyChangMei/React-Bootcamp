6406414c878b9bbf54c60a8aa70b67d380860b38
Sunny Chang Mei <mkaleader@gmail.com>
2019-05-18 18:47:14 -0400

    DS get()

---
+++ Data Structures/Singly Linked Lists/Get.js
@@ -0,0 +1,102 @@
+/**
+ * ! Get pseudocode
+ * 1. this function should accept an index
+ * 2. if th index is less than zero or greater than or equal to the length of the list, return null
+ * 3. loop through the list until you reach the index and return the node at the specific index
+ */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  // traverse() {
+  //   let current = this.head;
+  //   while (current) {
+  //     console.log(current.val);
+  //     current = current.next;
+  //   }
+  // }
+  pop() {
+    if (this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (this.head) return undefined;
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+}
+
+const list = new SinglyLinkedList();
+
+console.log(list.push('Hello'));
+console.log(list.push('Goodbye'));
+console.log(list.push('99'));
+console.log(list.pop());
+console.log(list.shift());
+console.log(list.unshift());
