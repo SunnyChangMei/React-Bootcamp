2f8655f5075479e457a84d2b5bdba53cb1f14927
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-23 20:16:49 -0400

    import TodoApp

--- react-hooks/react-hooks-todos/src/App.js
+++ react-hooks/react-hooks-todos/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import './App.css';
+import TodoApp from './components/TodoApp';
 
 function App() {
-  return (
-    <div className="App">
-     <TodoApp/>
-    </div>
-  );
+  return <TodoApp />;
 }
 
 export default App;
