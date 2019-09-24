e8b013f588ace889c3f88b3247c974d217e8c692
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-23 21:20:59 -0400

    add removeTodo, toggleTodo

--- react-hooks/react-hooks-todos/src/components/TodoList.jsx
+++ react-hooks/react-hooks-todos/src/components/TodoList.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Paper, List, ListItem, ListItemText } from '@material-ui/core';
+import {
+  Paper,
+  List,
+  Divider
+} from '@material-ui/core';
+import Todo from './Todo';
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos, removeTodo, toggleTodo }) => (
   <Paper>
     <List>
-      {todos.map(({ todo, id, task, completed }) => (
+      {todos.map(({ task, id, completed }) => (
         <>
-          <ListItem>
-            <ListItemText key={id}>{task}</ListItemText>
-          </ListItem>
+          <Todo key={id} task={task} completed={completed} removeTodo={removeTodo} id={id} toggleTodo={toggleTodo} />
+
+          <Divider />
         </>
       ))}
     </List>
