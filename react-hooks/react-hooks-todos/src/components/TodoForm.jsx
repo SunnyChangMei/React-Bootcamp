1b980e5d1e0e9226f63a99147b65c4e55521dc4d
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-23 20:36:16 -0400

    complete TodoForm

--- react-hooks/react-hooks-todos/src/components/TodoForm.jsx
+++ react-hooks/react-hooks-todos/src/components/TodoForm.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Paper, TextField } from '@material-ui/core';
+import { Paper, TextField, Button } from '@material-ui/core';
+import useInputState from '../hooks/useInputState';
 
-const TodoForm = () => (
-  <Paper>
-    <TextField  />
-  </Paper>
-);
+const TodoForm = ({ addTodo }) => {
+  const [value, handleChange, reset] = useInputState('');
+
+  return (
+    <Paper>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          addTodo(value);
+          reset();
+        }}>
+        <TextField value={value} onChange={handleChange} />
+      </form>
+      {/* <Button onClick={}>
+        Submit
+      </Button> */}
+    </Paper>
+  );
+};
 
 export default TodoForm;
