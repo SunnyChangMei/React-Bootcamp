8e0b64e3661a30ef3f0fdd685410b0b97691da73
Sunny Chang Mei <mkaleader@gmail.com>
2019-09-23 21:33:57 -0400

    check TodoList length or null

--- react-hooks/react-hooks-todos/src/components/TodoList.jsx
+++ react-hooks/react-hooks-todos/src/components/TodoList.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import {
-  Paper,
-  List,
-  Divider
-} from '@material-ui/core';
+import { Paper, List, Divider } from '@material-ui/core';
 import Todo from './Todo';
 
-const TodoList = ({ todos, removeTodo, toggleTodo }) => (
-  <Paper>
-    <List>
-      {todos.map(({ task, id, completed }) => (
-        <>
-          <Todo key={id} task={task} completed={completed} removeTodo={removeTodo} id={id} toggleTodo={toggleTodo} />
-
-          <Divider />
-        </>
-      ))}
-    </List>
-  </Paper>
-);
+const TodoList = ({ todos, removeTodo, toggleTodo }) => {
+  if (todos.length) {
+    return (
+      <Paper>
+        <List>
+          {todos.map(({ task, id, completed }) => (
+            <>
+              <Todo
+                key={id}
+                task={task}
+                completed={completed}
+                removeTodo={removeTodo}
+                id={id}
+                toggleTodo={toggleTodo}
+              />
+              <Divider />
+            </>
+          ))}
+        </List>
+      </Paper>
+    );
+  }
+  return null;
+};
 
 export default TodoList;
